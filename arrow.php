4709e665015d5f61e3dd605e946c07fcb479da9d
BiznesMeM <kosmosdrago@gmail.com>
2023-10-23 15:30:53 +0300

    Arrow left icon, Where have phone, message

---
+++ arrow.php
@@ -0,0 +1,45 @@
+<div class="call">
+
+<div class="svg-arrow" id="arrow">
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 20 20" fill="none" class="arrow-svg-svg">
+            <path d="M6.02255 9.37608L10.0001 5.5M10.0001 5.5L13.9776 9.37608M10.0001 5.5V14.0165M15.6251 19L4.375 19C2.51104 19 1 17.489 1 15.625L1 4.375C1 2.51104 2.51104 1 4.375 1L15.6251 1C17.4891 1 19.0001 2.51104 19.0001 4.375V15.625C19.0001 17.489 17.4891 19 15.6251 19Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+    </div>
+
+        <div class="arrow__item" id="contact">
+
+        <div class="svg-phone">
+
+            <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" class="svg-phone-phone">
+                <path d="M8.40005 3.59992L10 5.99992H14L15.6 3.59992M4.80005 4.7999V19.1999C4.80005 20.5254 5.87457 21.5999 7.20005 21.5999H16.8C18.1255 21.5999 19.2 20.5254 19.2 19.1999V4.79992C19.2 3.47443 18.1255 2.39992 16.8001 2.39992L7.20005 2.3999C5.87457 2.3999 4.80005 3.47442 4.80005 4.7999Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            </a>
+
+        </div>
+
+        <div class="svg-telegram">
+
+        <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" class="svg-telegram-telegram">
+                <path d="M21.0703 2.92961L10.4063 13.5936M3.271 8.23529L19.8769 2.47406C20.8995 2.11927 21.8807 3.1004 21.5259 4.12303L15.7646 20.7289C15.37 21.8665 13.7725 21.8977 13.3337 20.7764L10.6968 14.0377C10.5651 13.7011 10.2988 13.4348 9.96226 13.3031L3.22354 10.6662C2.10219 10.2275 2.13338 8.62997 3.271 8.23529Z" stroke="white" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+        </a>
+
+        </div>
+
+        <div class="svg-watshapp">
+
+        <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" class="svg-watshapp-wathsapp">
+                <path d="M4.99983 11C4.99983 6.58172 8.58125 3 12.9992 3C17.4171 3 20.9985 6.58172 20.9985 11C20.9985 12.15 20.7558 13.2434 20.319 14.2316L21 18.9992L16.9146 17.9778C15.7572 18.6287 14.4215 19 12.9992 19M3.00095 16C3.00095 16.7188 3.15258 17.4021 3.4256 18.0198L3 20.9995L5.55315 20.3611C6.27643 20.768 7.11115 21 8.00005 21C10.761 21 12.9992 18.7614 12.9992 16C12.9992 13.2386 10.761 11 8.00005 11C5.23912 11 3.00095 13.2386 3.00095 16Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </a>
+
+        </div>
+
+        </div>
+
+    </div>
